455d5d2ce06060d28f97b41722115e68af93f23b
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-08-12 05:22:56 -0700

    wrote a few tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,13 +3,22 @@
 
 import unittest
 from models.base_model import BaseModel
-
+from datetime import datetime
 
 class TestBaseModel(unittest.TestCase):
     def test_id_is_string(self):
         base_model = BaseModel()
         self.assertIsInstance(base_model.id, str)
-    def test_created_at_ISO(self):
+    def test_unique_id(self):
+        bm_1 = BaseModel()
+        bm_2 = BaseModel()
+        self.assertNotEqual(bm_1.id, bm_2.id)
+    def test_created_at_is_datetime(self):
         base_model = BaseModel()
-        self.assertIsInstance(base_model.created_at, ISO)
-    
+        self.assertIs(type(base_model.created_at), datetime)
+    def test_to_dict(self):
+        bm = BaseModel();
+        bm.name = "My First Model"
+        bm.my_number = 89
+        bm_json = bm.to_dict()
+        self.assertIs(type(bm_json), dict)
