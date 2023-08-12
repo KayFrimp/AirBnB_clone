2f80b9975c931b9e60dfee2545ba79b9f20861a6
Debby696 <idowuopeyemi696@gmail.com>
2023-08-12 04:53:24 -0700

    testcase

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,8 +2,14 @@
 """base_model.py module"""
 
 import unittest
-from models.base_model import Basemodel
+from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
-    pass
+    def test_id_is_string(self):
+        base_model = BaseModel()
+        self.assertIsInstance(base_model.id, str)
+    def test_created_at_ISO(self):
+        base_model = BaseModel()
+        self.assertIsInstance(base_model.created_at, ISO)
+    
